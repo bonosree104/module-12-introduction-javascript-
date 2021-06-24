0e74439973abc6a921a3e98f27598f8839730bac
bonosree104 <bonosreeela104@gmail.com>
2021-06-24 23:42:23 +0600

    my javascript

---
+++ hello.js
@@ -0,0 +1,23 @@
+var mangoPrice = 12;
+var umrellaPrice =33;
+var bananaPrice = 50;
+console.log(typeof bananaPrice);
+console.log(typeof mangoPrice);
+console.log(typeof umrellaPrice);
+var name= "bonosree";
+var price= "karim";
+console.log(typeof name);
+console.log(typeof price);
+
+var weather =true;
+var forecast = false;
+var car =true;
+console.log(typeof weather);
+console.log(typeof forecast);
+console.log(typeof car);
+
+
+var promise = "i promise myself to work hard to become a solopreneur";
+console.log(promise.toUpperCase());
+console.log(promise.indexOf("myself"));
+console.log(promise.split("myself"));
