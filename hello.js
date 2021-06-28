f506181e481615b0c48d38a9b1a6f4639c3b3190
bonosree104 <bonosreeela104@gmail.com>
2021-06-28 21:14:13 +0600

    my website

--- hello.js
+++ hello.js
@@ -21,3 +21,28 @@ var promise = "i promise myself to work hard to become a solopreneur";
 console.log(promise.toUpperCase());
 console.log(promise.indexOf("myself"));
 console.log(promise.split("myself"));
+
+var num1 = "12.9999";
+var num2 =33.5;
+//num2 = parseInt(num2);/
+var total = num1=num2;
+total = total.toFixed(0);
+console.log(total);
+
+var name1 = "justine";
+var name2 ="biber";
+var result = name1+   "          " +name2;
+console.log(result);
+
+var absolute = -6;
+var absoluteNumber = Math.abs(absolute);
+console.log(absoluteNumber);
+
+
+var biscuitPrice = 10;
+if( biscuitPrice >9){
+ console.log("i will eat  rice");
+}
+else{
+console.log("i will eat veggies");
+}
